e24549a12dd8e44debb1d2214cc0021255db6efd
Uba Kelechi Michael <kcsplace20@gmail.com>
2021-08-22 14:32:27 +0100

    Initial commit

---
+++ toggleScript.js
@@ -0,0 +1,20 @@
+const toggleName = () => {
+
+    var nameDisplay = document.getElementById("my-name");
+    var nameButton = document.getElementById("name-button");
+    var resumeButton = document.getElementById("resume-button");
+
+    if (nameDisplay.style.display === "none") {
+        nameDisplay.style.display = "block";
+        nameButton.innerHTML = "Hide Name";
+        nameButton.style.backgroundColor = "red";
+        nameButton.style.color = "white";
+        resumeButton.style.display = "block";
+    } else {
+        nameDisplay.style.display = "none";
+        nameButton.innerHTML = "Show Name";
+        nameButton.style.backgroundColor = "green";
+        nameButton.style.color = "white";
+        resumeButton.style.display = "none";
+    }
+}
